44e9c0088bfff8a44163f66c30dfd3f25d2a41aa
lucas <lucascoronilla@hotmail.com>
2022-06-25 00:31:24 +0200

    a

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LazyloadDirective } from './directives/lazyload.directive';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LazyloadDirective
+  ],
   imports: [
     CommonModule
   ]
